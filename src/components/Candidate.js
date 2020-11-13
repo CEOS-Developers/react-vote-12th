e36baacd16c14b7b5825c780955f4f622d8b62f9
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-11-13 14:59:39 +0900

    Add getVote function use axios

--- src/components/Candidate.js
+++ src/components/Candidate.js
@@ -1,14 +1,65 @@
 import React from 'react';
 import styled from 'styled-components';
+import { url } from './url';
+import axios from 'axios';
 
-const Candidate = ({ voteCandidate }) => {
-  return <Wrapper>{voteCandidate}</Wrapper>;
+const Candidate = ({ candidateName, voteCount, id, getCandidates }) => {
+  const getVote = async (id) => {
+    try {
+      const vote = await axios.get(`${url}/vote`, {
+        params: {
+          id: id,
+        },
+      });
+      getCandidates();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const onClick = () => {
+    getVote(id);
+  };
+  return (
+    <Wrapper>
+      <CandidateName>{candidateName}</CandidateName>
+      <VoteCount>{voteCount}</VoteCount>
+      <VoteButton onClick={onClick}>투표</VoteButton>
+    </Wrapper>
+  );
 };
 
 export default Candidate;
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
   margin: 20px;
+  font-size: 22px;
+  font-weight: 600;
+`;
+const CandidateName = styled.div`
+  width: 150px;
+`;
+const VoteCount = styled.div`
+  display: flex;
+  flex: 0 0 50px;
+  justify-content: flex-end;
+`;
+
+const VoteButton = styled.div`
+  height: 30px;
+  width: 50px;
+  padding-top: 5px;
+  margin-left: 20px;
+  border: 1px solid black;
   font-size: 20px;
-  font-weight: 500;
+  border-radius: 10px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  &:hover {
+    background-color: blue;
+    color: white;
+  }
 `;
