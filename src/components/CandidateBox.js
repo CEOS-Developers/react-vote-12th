469b2dbaf2a4e92f1e0fc89d432c5d432b6968ba
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-11-13 15:00:16 +0900

    Add getCandidates function use axios

--- src/components/CandidateBox.js
+++ src/components/CandidateBox.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Candidate from './Candidate';
-import { CANDIDATE_DUMMY } from './candidatedummy';
+import axios from 'axios';
+import { url } from './url';
 
 const CandidateBox = () => {
+  const [voteCandidate, setVoteCandidate] = useState('');
+  const getCandidates = async () => {
+    try {
+      const Candidates = await axios.get(`${url}/candidates`);
+      setVoteCandidate(Candidates.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    getCandidates();
+  }, []);
+
   return (
     <Wrapper>
-      {CANDIDATE_DUMMY.map((c, index) => {
-        return <Candidate key={index} voteCandidate={c} />;
-      })}
+      {voteCandidate &&
+        voteCandidate
+          .sort((a, b) => {
+            return b.voteCount - a.voteCount;
+          })
+          .map((c, index) => {
+            return (
+              <Candidate
+                key={index}
+                id={c.id}
+                candidateName={c.name}
+                voteCount={c.voteCount}
+                getCandidates={getCandidates}
+              />
+            );
+          })}
     </Wrapper>
   );
 };
